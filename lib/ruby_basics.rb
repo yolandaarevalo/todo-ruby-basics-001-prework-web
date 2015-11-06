d0727ce82d046bffe225553de00f4c1958e1af74
yolandaarevalo <yoliarevalo53@gmail.com>
2015-11-06 17:15:21 +0000

    this lab is done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
-def division(num1, num2)
- 
+def division(num1=42, num2=7)
+ num1 / num2 
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  my_variable = name
 end
 
-def argue
-  
+def argue(phrase = "I'm right and you are wrong")
+  puts phrase 
+  return phrase
 end
 
-def greeting
-
+def greeting( greeting, name)
+  greeting = greeting
+  puts "Hi there, #{name}"
 end
 
-def return_a_value
-
+def return_a_value (phrase = "Nice")
+puts phrase
+  return phrase
 end
 
-def last_evaluated_value
-
+def last_evaluated_value (phrase ="expert")
+puts phrase
+  return phrase
 end
 
-def pizza_party
-
+def pizza_party (phrase= "cheese")
+  return phrase
 end
+pizza_party ("pepperoni")
